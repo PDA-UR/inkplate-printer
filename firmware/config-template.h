a3af6d8f925c99f1fc687f3fb709a94974dd715c
Alexander Weichart <>
2022-09-21 10:42:53 +0200

    added: config template

---
+++ firmware/config-template.h
@@ -0,0 +1,11 @@
+#include "Inkplate.h"
+
+const char SSID[] = "";    // Your WiFi SSID
+const char *PASSWORD = ""; // Your WiFi password
+const char HOST[] = ""; // Printer host
+
+IPAddress local_ip  =   IPAddress(192, 168, 2, 103);
+IPAddress gateway   =   IPAddress(192, 168, 2, 1);
+IPAddress subnet    =   IPAddress(255, 255, 255, 0);
+IPAddress dns1      =   IPAddress(1, 1, 1, 1);
+IPAddress dns2      =   IPAddress(1, 0, 0, 1);
